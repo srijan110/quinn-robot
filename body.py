53928eb4c5163d20b92d55e45eebaf6ad9b30396
Srijan Purkait <srijanpurkait2011@gmail.com>
2025-11-13 15:50:40 +0000

    Adds Walking Front

--- body.py
+++ body.py
@@ -10,7 +10,7 @@ def minmax(x, p=180, n=0):
 def init_servo():
     kit.servo[0].set_pulse_width_range(450, 2600)
     kit.servo[1].set_pulse_width_range(450, 2600)
-    kit.servo[2].set_pulse_width_range(500, 2600)
+    kit.servo[2].set_pulse_width_range(500, 2500)
     kit.servo[3].set_pulse_width_range(450, 2500)
     kit.servo[4].set_pulse_width_range(500, 2600)
     kit.servo[5].set_pulse_width_range(500, 2600)
@@ -18,8 +18,8 @@ def init_servo():
 
 def set_servo_angles(leg_one, leg_two):
     kit.servo[0].angle = minmax(leg_one[0] + 10)  # 85
-    kit.servo[1].angle = minmax(leg_two[0] + 5)  # 95
-    kit.servo[2].angle = minmax(leg_one[1] + 90 - 5)  # 85
-    kit.servo[3].angle = minmax(leg_two[1] + 90 - 5)  # 85
-    kit.servo[4].angle = minmax(leg_one[2] + 180, n=60, p=120)  # 95
-    kit.servo[5].angle = minmax(leg_two[2] + 180, n=60, p=120)  # 90
+    kit.servo[1].angle = minmax(leg_two[0])  # 95
+    kit.servo[2].angle = minmax(leg_one[1] + 90 + 10)  # 85
+    kit.servo[3].angle = minmax(leg_two[1] + 90 - 10)  # 85
+    kit.servo[4].angle = minmax(leg_one[2] + 180 - 7, n=60, p=120)  # 95
+    kit.servo[5].angle = minmax(leg_two[2] + 180 + 8, n=60, p=120)  # 90
--- walk.py
+++ walk.py
@@ -1,31 +1,51 @@
-from maths import degrees, get_coords, get_angles
-from body import init_servo, set_servo_angles
 from time import sleep
+from maths import degrees
+from body import init_servo, set_servo_angles
+from maths import get_x_zmp, get_angles, PID_Controller, minmax, get_coords
+from sensor import ACCEL_out, GYRO_out, ADC_out
+from math import radians
 
-if __name__ != "__main__":
-    exit()
 
-# example test code
+def walk_front(ix=1, iy=9, fx=-2, fy=9, hip_offset=-10, ankle_offset=0, step_duration=0.2, delay=0.01, dx=-1, dy=9, steps=1):
+    left_leg, right_leg = ([degrees(x) for x in get_angles(dx, dy, 5, 5, 3)], [degrees(x) for x in get_angles(-1, 9, 5, 5, 3)])
+    left_leg[0] = right_leg[0] = left_leg[0] + hip_offset
+    set_servo_angles(left_leg, right_leg)
 
-delay = 0.01
-index = 0
-total = round(0.25/delay)
+    index = 0
+    total = round(step_duration/delay)
+    swing = True
 
-init_servo()
-
-swing = True
-
-ix, iy = -3, 9
-fx, fy = 1, 9
-
-while True:
-    x, y = get_coords(index, total, swing, ix, iy, fx, fy, -1)
-    lx, ly = get_coords(index, total, not swing, fx, fy, ix, iy, -1)
-    # print(x, y)
-    set_servo_angles([degrees(x) for x in get_angles(x, y, 5, 5, 3)], [degrees(x) for x in get_angles(lx, ly, 5, 5, 3)])
+    step_index = 0
     sleep(delay)
-    index += 1
-    if index >= total+1 or index == total+1:
-        index = 0
-        swing = not swing
-        ix, iy, fx, fy = fx, fy, ix, iy
+
+    while step_index != steps:
+        lx, ly = get_coords(index, total, swing, ix, iy, fx, fy, -2)
+        rx, ry = get_coords(index, total, not swing, fx, fy, ix, iy, -2)
+
+        left_leg = [degrees(x) for x in get_angles(lx, ly, 5, 5, 3)]
+        right_leg = [degrees(x) for x in get_angles(rx, ry, 5, 5, 3)]
+
+        print(left_leg)
+
+        left_leg[0] = minmax(left_leg[0] + hip_offset - 15)
+        right_leg[0] = minmax(right_leg[0] + hip_offset - 15)
+
+        set_servo_angles(left_leg, right_leg)
+        sleep(delay)
+
+        index += 1
+        if index >= total+1 or index == total+1:
+            index = 0
+            step_index += 1
+            swing = not swing
+            ix, iy, fx, fy = fx, fy, ix, iy
+
+    left_leg, right_leg = ([degrees(x) for x in get_angles(dx, dy, 5, 5, 3)], [degrees(x) for x in get_angles(-1, 9, 5, 5, 3)])
+    left_leg[0] = right_leg[0] = left_leg[0] + hip_offset
+    set_servo_angles(left_leg, right_leg)
+
+
+if __name__ == "__main__":
+    init_servo()
+
+    walk_front(steps=50)
